107450854a22607725d4fb3b31521f4b3b0f2b6c
AnastasiyaPilushina <pilushina22@gmail.com>
2021-01-21 14:24:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 10_600;
+        int miles = 20;
+        int total = ticket / miles;
+        System.out.println(total);
+
+    }
+}
